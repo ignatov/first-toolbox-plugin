196c74431d3656b287d38a25cf0beb83f0e9dc7a
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2023-03-24 16:14:52 +0100

    id with dot

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,36 +20,37 @@ dependencies {
     implementation(files("/Users/ignatov/src/toolbox/feature/gateway-plugin-api/build/classes/java/main/"))
 }
 
-val generateJson = tasks.register<DefaultTask>("generateJson") {
-    val project = project.rootProject
-
-    val ext = Extension(
-        id = project.name,
-        version = project.version.toString(),
-        meta = Meta(
-            readableName = "Chunga Change",
-            description = "Chunga Changa plugin",
-            vendor = "JetBrains",
-        ),
-        compatibleVersionRange = CompatibleVersionRange(
-            from = "1.1",
-            to = "1.2",
-        )
-    )
-    val outputFile = File("${project.buildDir}/generated-json/extension.json")
-    outputFile.parentFile.mkdirs()
-    outputFile.writeText(ext.toJson())
-}
+//val generateJson = tasks.register<DefaultTask>("generateJson") {
+//    val project = project.rootProject
+//
+//    val ext = Extension(
+//        id = project.name,
+//        version = project.version.toString(),
+//        meta = Meta(
+//            readableName = "Chunga Change",
+//            description = "Chunga Changa plugin",
+//            vendor = "JetBrains",
+//        ),
+//        compatibleVersionRange = CompatibleVersionRange(
+//            from = "1.1",
+//            to = "1.2",
+//        )
+//    )
+//    val outputFile = File("${project.buildDir}/generated-json/extension.json")
+//    outputFile.parentFile.mkdirs()
+//    outputFile.writeText(ext.toJson())
+//}
 
 version = "0.2"
 
 tasks.register<Zip>("createZip") {
-    dependsOn(tasks.assemble, generateJson)
+    dependsOn(tasks.assemble)
     val compileClasspath = configurations.compileClasspath
     from(compileClasspath.get().files.filter { it.extension == "jar" })
     val name = "${rootProject.name}-${rootProject.version}"
     from(file("${rootProject.buildDir}/libs/$name.jar"))
-    from(file("${rootProject.buildDir}/generated-json/extension.json"))
+    from(file("src/main/resources/icon.svg"))
+    from(file("src/main/resources/extension.json"))
     archiveFileName.set("$name.zip")
     destinationDirectory.set(file("$buildDir/libs"))
 }
